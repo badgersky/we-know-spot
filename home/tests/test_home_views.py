fe207654b389e48354c4a332d3d5e7e71eb1e595
badgersky <szymonek.ptasznik@gmail.com>
2023-06-03 14:01:50 +0200

    create test home view

---
+++ home/tests/test_home_views.py
@@ -0,0 +1,11 @@
+from django.urls import reverse
+
+
+def test_home_page(client):
+    url = reverse('home:home')
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert 'Home Page' in response.content.decode('utf-8')
+    
