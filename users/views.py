a463d200955a0c2420517feb24485b68a99b7f9f
badgersky <szymonek.ptasznik@gmail.com>
2023-06-03 15:05:42 +0200

    create login view and template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from users.forms import LoginForm
+
+
+class LoginView(View):
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
+            )
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('home:home'))
+
+        return render(request, 'users/login.html', {'form': form})
