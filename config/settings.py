56efb34a0fe11daf0b1dbf7a56a6fe068a53aefa
badgersky <szymonek.ptasznik@gmail.com>
2023-06-03 15:34:17 +0200

    update base.html

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from config import _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -44,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third party
-    'debug_toolbar',
     'crispy_forms',
     'crispy_bootstrap5',
+    'debug_toolbar',
 
     # my apps
     'home.apps.HomeConfig',
@@ -84,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -108,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -119,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
