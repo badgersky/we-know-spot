228b1967b21c7435c6bb82e5053fbf214e188779
badgersky <szymonek.ptasznik@gmail.com>
2023-06-04 16:30:28 +0200

    update create-spot-form, update create-spot-template

--- spots/forms.py
+++ spots/forms.py
@@ -1,17 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from spots.models import Spot, Tag, Province
 
 
 class CreateSpotForm(forms.ModelForm):
     tags = forms.MultipleChoiceField(
-        choices=Tag.objects.all()
+        choices=[(tag.pk, tag) for tag in Tag.objects.all()]
     )
     province = forms.ChoiceField(
-        choices=Province.objects.all()
+        choices=[(province.pk, province) for province in Province.objects.all()]
     )
 
     class Meta:
         model = Spot
         fields = ('name', 'province', 'longitude', 'latitude', 'tags', 'photo')
-        
+
+    def clean_province(self):
+        prov_pk = self.cleaned_data.get('province')
+        if not prov_pk:
+            raise ValidationError(f'Please select province')
+
+        return Province.objects.get(pk=int(prov_pk))
