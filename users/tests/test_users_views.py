a843b91191e177ccca78c0150ac3623e91714f17
badgersky <szymonek.ptasznik@gmail.com>
2023-06-04 11:34:36 +0200

    create tests for registration view

--- users/tests/test_users_views.py
+++ users/tests/test_users_views.py
@@ -46,3 +46,86 @@ def test_logout_view_user_not_logged(client, user):
     assert redirect.status_code == 302
     assert response.status_code == 200
     assert 'Login' in response.content.decode('utf-8')
+
+
+def test_register_view_get(client):
+    url = reverse('users:register')
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert 'Register' in response.content.decode('utf-8')
+
+
+def test_registration_view_post(client, django_user_model):
+    url = reverse('users:register')
+    data = {
+        'username': 'test_user',
+        'password': 'test_password',
+        'confirm_password': 'test_password'
+    }
+    num_of_users = django_user_model.objects.count()
+
+    redirect = client.post(url, data)
+    response = client.get(redirect.url)
+    num_of_users_after = django_user_model.objects.count()
+
+    assert redirect.status_code == 302
+    assert response.status_code == 200
+    assert 'Login' in response.content.decode('utf-8')
+    assert num_of_users == num_of_users_after - 1
+
+
+def test_registration_view_different_confirm_password(client, django_user_model):
+    url = reverse('users:register')
+    data = {
+        'username': 'test_user',
+        'password': 'test_password',
+        'confirm_password': 'password_test'
+    }
+    num_of_users = django_user_model.objects.count()
+
+    response = client.post(url, data)
+    num_of_users_after = django_user_model.objects.count()
+
+    assert response.status_code == 200
+    assert 'Register' in response.content.decode('utf-8')
+    assert 'Passwords don`t match' in response.content.decode('utf-8')
+    assert num_of_users == num_of_users_after
+
+
+def test_registration_view_user_already_exist(client, user, django_user_model):
+    url = reverse('users:register')
+    data = {
+        'username': user.username,
+        'password': 'test_password',
+        'confirm_password': 'test_password',
+    }
+    num_of_users = django_user_model.objects.count()
+
+    response = client.post(url, data)
+    num_of_users_after = django_user_model.objects.count()
+
+    assert response.status_code == 200
+    assert 'Register' in response.content.decode('utf-8')
+    assert 'Try using different username' in response.content.decode('utf-8')
+    assert num_of_users == num_of_users_after
+
+
+def test_registration_view_weak_password(client, django_user_model):
+    url = reverse('users:register')
+    data = {
+        'username': 'test_user',
+        'password': 'a',
+        'confirm_password': 'a',
+    }
+    num_of_users = django_user_model.objects.count()
+
+    response = client.post(url, data)
+    num_of_users_after = django_user_model.objects.count()
+
+    assert response.status_code == 200
+    assert 'Register' in response.content.decode('utf-8')
+    assert 'This password is too short. It must contain at least 8 characters.' in response.content.decode('utf-8')
+    assert num_of_users == num_of_users_after
+    
