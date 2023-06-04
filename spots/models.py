ac8afd394181a331de8c4bd38e1f8d07bc5fd2ea
badgersky <szymonek.ptasznik@gmail.com>
2023-06-04 12:35:34 +0200

    create tag and province model, register models

--- spots/models.py
+++ spots/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100)
+    date_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tag_name
+
+
+class Province(models.Model):
+    province_name = models.CharField(max_length=100)
+    main_city = models.CharField(max_length=100)
+    date_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.province_name
