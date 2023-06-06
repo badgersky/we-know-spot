4cd8029df0a5344079cc0e5b70c1816a9dfd89ab
badgersky <szymonek.ptasznik@gmail.com>
2023-06-06 18:36:22 +0200

    create endpoint for like spot view

--- spots/urls.py
+++ spots/urls.py
@@ -7,4 +7,5 @@ app_name = 'spots'
 urlpatterns = [
     path('create/', views.CreateSpotView.as_view(), name='create'),
     path('list/', views.ListSpotsView.as_view(), name='list'),
+    path('like/', views.LikeSpot.as_view(), name='like'),
 ]
