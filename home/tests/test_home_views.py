93e395e4e3700e5ad76a457398f358ed3a0b28b6
badgersky <szymonek.ptasznik@gmail.com>
2023-06-09 17:38:11 +0200

    update tests

--- home/tests/test_home_views.py
+++ home/tests/test_home_views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse
 
 
-def test_home_page(client):
+def test_home_page(client, db, django_user_model):
     url = reverse('home:home')
 
     response = client.get(url)
 
     assert response.status_code == 200
-    assert 'Home Page' in response.content.decode('utf-8')
+    assert 'About' in response.content.decode('utf-8')
+    assert str(django_user_model.objects.count()) in response.content.decode('utf-8')
+    
